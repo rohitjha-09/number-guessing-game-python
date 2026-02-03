6e11cdada51977b2d4e84a62f2f306c570c7230b
Rohit Jha <sumitk66684@gmail.com>
2026-02-03 16:49:52 +0530

    Add files via upload

---
+++ number_guessing_game.py
@@ -0,0 +1,20 @@
+import random
+boom=random.randint(1,100)
+
+guess=int(input("guess the number between 1 to 100🤔:"))
+counter=1
+
+while guess!=boom:
+    if guess<boom:
+        print("wrong guess higher👎")
+    else:
+        print("wrong guess lower👎")    
+
+    guess=int(input("guess the number🤔:"))     
+    counter+=1
+else:
+        print("correct guess👊")   
+        print("attempts👍:",counter)
+ 
+
+
